572f039ed44146f4fffdc3316b105484f2b26807
ilmari <ilmari.luukkainen@gmail.com>
2026-09-02 17:00:01 +0300

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Jos sivustosi osoite on muotoa käyttäjä.github.io/aktiiva-website,
+  // poista alla olevalta riviltä kommenttimerkit // ja tarkista repositoriosi nimi:
+  // basePath: '/aktiiva-website',
 };
 
 export default nextConfig;
